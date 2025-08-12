c13d86f39bb016e06ead07e40ce830cf71460017
Alomgir-Bhuiyan <alomgirbhuiyan16@gmail.com>
2025-08-12 05:33:27 +0000

    Updated setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 rm -rf $HOME/.config/fish
 cp -r ./.config/fish $HOME/.config/
 rm -rf $HOME/.poshthemes
 cp -r ./.poshthemes $HOME/
 chmod u+rw $HOME/.poshthemes/*.omp.*
 cp -r ./.zshrc $HOME/
-cd ..
-#rm -rf tiwahu-config
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$DIR/.."
+rm -rf "$DIR"
